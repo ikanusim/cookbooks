679c147b4546c93114a4f88c7d47414f3a36d17c
Daniel Hüsch <daniel.huesch@gmail.com>
2013-02-13 05:53:50 +0100

    Add dummy recipes.

---
+++ custom/recipes/deploy.rb
@@ -0,0 +1,5 @@
+ruby_block 'custom deploy' do
+  block do
+    Chef::Log.info 'dhdbg custom deploy'
+  end
+end
---
+++ custom/recipes/setup.rb
@@ -0,0 +1,5 @@
+ruby_block 'custom setup' do
+  block do
+    Chef::Log.info 'dhdbg custom setup'
+  end
+end
